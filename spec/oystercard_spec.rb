385fc2aba269c6f03d91dc45a19ef48787065231
Yusuf <yakhaliq01@gmail.com>
2021-06-07 15:21:24 +0100

    challenge5: top_up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,13 @@
 require 'oystercard'
 
 describe Oystercard do  
-end
+  it 'checks if no money on card' do
+    expect(subject.balance).to eq(0)
+  end
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+    it 'can top up the balance' do
+      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+    end
+  end
+end 
