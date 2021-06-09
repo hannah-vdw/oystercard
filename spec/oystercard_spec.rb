a14b06d7ad6776e98b0e505c2b76c7f3b5e6fb76
Hannah <hannah.v.d.wijngaard@gmail.com>
2021-06-09 14:58:46 +0100

    completed challenge 10 - refactor

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,20 +1,21 @@
 require 'oystercard'
 
 describe Oystercard do  
+  subject(:oystercard) { described_class.new }
 
   it 'checks if no money on card' do
-    expect(subject.balance).to eq(0)
+    expect(oystercard.balance).to eq(0)
   end
 
   describe '#top_up' do
     it { is_expected.to respond_to(:top_up).with(1).argument }
     it 'can top up the balance' do
-      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+      expect{ oystercard.top_up 1 }.to change{ oystercard.balance }.by 1
     end
     it 'doesn\'t allow excession of £90' do
       maximum_balance = Oystercard::MAXIMUM_BALANCE
-      subject.top_up(maximum_balance)
-      expect{ subject.top_up(1) }.to raise_error "£#{maximum_balance} limit"
+      oystercard.top_up(maximum_balance)
+      expect{ oystercard.top_up(1) }.to raise_error "£#{maximum_balance} limit"
     end
   end
 
@@ -22,13 +23,13 @@ describe Oystercard do
   #   it { is_expected.to respond_to(:deduct).with(1).argument }
   # end
   # it 'can deduct the balance' do
-  #   expect{ subject.deduct 1 }.to change{ subject.balance }.by -1
+  #   expect{ oystercard.deduct 1 }.to change{ oystercard.balance }.by -1
   # end
 
   # describe '#touch_in' do
   #   it { is_expected.to respond_to(:touch_in)}
   #   it 'doesn\'t allow travel if balance lower than £1' do
-  #     expect{ subject.touch_in}.to raise_error "Insufficient funds. Please top up"
+  #     expect{ oystercard.touch_in}.to raise_error "Insufficient funds. Please top up"
   #   end
   # end
 
@@ -37,9 +38,9 @@ describe Oystercard do
 
     it 'deducts fare' do
       minimum_fare = Oystercard::MINIMUM_FARE
-      subject.top_up(5)
-      subject.touch_in
-      expect { subject.touch_out }.to change{ subject.balance }.by(-minimum_fare)
+      oystercard.top_up(5)
+      oystercard.touch_in
+      expect { oystercard.touch_out }.to change{ oystercard.balance }.by(-minimum_fare)
     end
 
   end
@@ -47,18 +48,18 @@ describe Oystercard do
   describe '#in_journey?' do
     it { is_expected.to respond_to(:in_journey?)}
     it 'is initially not in a journey' do
-      expect(subject).not_to be_in_journey
+      expect(oystercard).not_to be_in_journey
     end
     it 'it can touch in' do
-      subject.top_up(1)
-      subject.touch_in
-      expect(subject).to be_in_journey
+      oystercard.top_up(1)
+      oystercard.touch_in
+      expect(oystercard).to be_in_journey
     end
     it "can touch out" do
-      subject.top_up(1)
-      subject.touch_in
-      subject.touch_out
-      expect(subject).not_to be_in_journey
+      oystercard.top_up(1)
+      oystercard.touch_in
+      oystercard.touch_out
+      expect(oystercard).not_to be_in_journey
     end
   end
 end 
